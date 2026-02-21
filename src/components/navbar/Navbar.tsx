31b5b4c3f1ab35a6dc18c2c3f0ecb1a2da109f73
faaraxcabdulaahi <barashoonline2021@gmail.com>
2026-02-21 16:11:03 +0300

    add nav tag which has some tailwind classes and border for the start of navbar designing

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Navbar = () => {
+  return <nav className="fixed top-0 z-50 w-full h-18 lg:h-24 bg-white border-b border-gray-200">
+    
+  </nav>;
+};
+
+export default Navbar;
