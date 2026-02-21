9bf51feaf17119c5bd6c040805464891e28e5011
faaraxcabdulaahi <barashoonline2021@gmail.com>
2026-02-21 16:03:06 +0300

    added core setup of the airbnb project

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import {Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "Airbnb Clone",
+  description: "Airbnb Clone Website",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${poppins.className}antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
